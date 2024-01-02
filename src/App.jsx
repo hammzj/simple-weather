489600c6dd933ecb220ac0b8d77da2790229ab3f
zjhamm304@gmail.com <zach.hamm@validic.com>
2024-01-02 12:41:23 -0500

    add:monospace font

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Box} from '@mui/material';
 import {RouterProvider} from "react-router-dom";
 import router from "./routes/router";
+import './fonts';
 import "./App.css";
 
 export default function App() {
---
+++ src/fonts.jsx
@@ -0,0 +1,4 @@
+import '@fontsource/roboto-mono/300.css';
+import '@fontsource/roboto-mono/400.css';
+import '@fontsource/roboto-mono/500.css';
+import '@fontsource/roboto-mono/700.css';
