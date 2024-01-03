a16e9305d4c1114eaa7dea9d059a47432b75643a
zjhamm304@gmail.com <zach.hamm@validic.com>
2024-01-03 16:12:16 -0500

    add:allow setting themes on cy.mount

--- cypress/support/component.js
+++ cypress/support/component.js
@@ -1,17 +1,3 @@
-// ***********************************************************
-// This example support/component.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.js using ES2015 syntax:
+import '../../src/fonts';
+import '../../src/index.css';
 import './commands'
