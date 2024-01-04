a4ce75217c2420f70fd081049d068832b205185f
zjhamm304@gmail.com <zach.hamm@validic.com>
2024-01-04 15:07:53 -0500

    add:tests for settings page; remove unused icons

--- cypress/component/components/settings.menu.cy.js
+++ cypress/component/components/settings.menu.cy.js
@@ -1,12 +1,11 @@
 import SettingsMenu from '../../../src/components/settings.menu';
 import SettingsMenuObject from '../../page_objects/components/settings.menu.object';
-import {OPEN_METEO_HOMEPAGE, GITHUB_AUTHOR_LINK} from "../../../src/constants";
 
 describe(SettingsMenu.name, function () {
     it('renders correctly', function () {
-        const SettingsMenuObject = new SettingsMenuObject();
+        const settingsMenuObject = new SettingsMenuObject();
 
         cy.mount(<SettingsMenu/>);
-        bottomNavBarObject.darkModeToggle.parents('label').should('have.text', 'Enable dark mode');
+        settingsMenuObject.darkModeToggle.parents('label').should('have.text', 'Enable dark mode');
     });
 });
