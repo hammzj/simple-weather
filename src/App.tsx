98a7cfe5789424d3a8ecbd7638df1de1a9f64c8f
Zachary Hamm <zjhamm304@gmail.com>
2024-01-05 15:45:24 -0500

    Dev/app/issue 18 (#22)
    * add:ts definitions for app and theme
    
    * add:use memoization w/ local storage for color mode (dark mode)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useMemo} from "react";
 import {RouterProvider} from "react-router-dom";
 import {Box, ThemeProvider, CssBaseline} from "@mui/material";
+import {Theme} from "@mui/material/styles";
 import ErrorBoundary from "./components/error.boundary";
 import ErrorPage from "./pages/error.page";
-import {getTheme} from "./theme";
+import {getTheme, PaletteMode} from "./theme";
 import {SETTINGS_KEY_NAMES} from './constants';
+import {ColorModeContext} from './contexts';
 import router from "./routes/router";
 import {TemperatureUnit, WindSpeedUnit, PrecipitationUnit} from './services/open_meteo_api/forecast_api';
-import {isNil} from "lodash";
+import {isNil, toString} from "lodash";
 
 //I bet there's a better way to handle this, but it'll work for now.
 //Maybe using a JSON object is better, but we'd still need to individually set the keys.
 const createDefaultSettings = () => {
     const defaultSettings = [
         {
-            key: SETTINGS_KEY_NAMES.DARK_MODE,
-            fallback: false,
+            key: SETTINGS_KEY_NAMES.COLOR_MODE,
+            fallback: 'light',
         },
         {
             key: SETTINGS_KEY_NAMES.TEMPERATURE_UNIT,
@@ -32,29 +34,42 @@ const createDefaultSettings = () => {
     ];
     for (const {key, fallback} of defaultSettings) {
         if (isNil(localStorage.getItem(key))) {
-            console.debug('No default found for setting. Default value was set for key:', key, fallback);
-            localStorage.setItem(key, fallback);
+            localStorage.setItem(key, toString(fallback));
+            console.debug('No default found for setting. Default value was set for key:', key, toString(fallback));
         }
     }
 }
 
-export default function App() {
-    const [theme, setTheme] = useState(getTheme());
+export default function App(): React.ReactElement {
+    const [mode, setMode] = React.useState<string>('light');
+
+    //Use local storage to set the color mode
+    const colorMode = React.useMemo(
+        () => ({
+            setColorMode: () => {
+                setMode(localStorage.getItem(SETTINGS_KEY_NAMES.COLOR_MODE) ?? 'light')
+            },
+        }),
+        [],
+    );
+    // Update the theme only if the mode changes
+    const theme = useMemo(() => getTheme(mode), [mode]);
 
     useEffect(() => {
         createDefaultSettings();
-        setTheme(getTheme());
     }, []);
 
     return (
-        <Box sx={{textAlign: 'center'}}>
+        <ColorModeContext.Provider value={colorMode}>
             <ThemeProvider theme={theme}>
                 <CssBaseline/>
-                <ErrorBoundary fallback={<ErrorPage/>}>
-                    <RouterProvider router={router}/>
-                </ErrorBoundary>
+                <Box sx={{textAlign: 'center'}}>
+                    <ErrorBoundary fallback={<ErrorPage/>}>
+                        <RouterProvider router={router}/>
+                    </ErrorBoundary>
+                </Box>
             </ThemeProvider>
-        </Box>
+        </ColorModeContext.Provider>
     );
 }
 
