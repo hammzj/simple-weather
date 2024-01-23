4f9494a1d5304b51b2099499a9c165dc27883773
zjhamm304@gmail.com <zach.hamm@validic.com>
2024-01-23 13:21:03 -0500

    add:new weather icons

---
+++ src/weather_icons/index.jsx
@@ -0,0 +1,9 @@
+//Erik Flowers /Weather Icons:
+//@see https://erikflowers.github.io/weather-icons
+import "./font/weathericons-regular-webfont.eot";
+import "./font/weathericons-regular-webfont.svg";
+import "./font/weathericons-regular-webfont.ttf";
+import "./font/weathericons-regular-webfont.woff";
+import "./font/weathericons-regular-webfont.woff2";
+
+import "./styles/weather-icons.css";
